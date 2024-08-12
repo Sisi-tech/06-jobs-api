22aae505c72759f5c0e12fbe7ca98cdf0ce19005
Sisi Wang <sisiwang242@gmail.com>
2024-08-12 00:16:35 -0400

    week9

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+<<<<<<< HEAD
+=======
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+// connectDB 
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
+
+>>>>>>> ef817e3 (assignment for week9 - Jobs-api-part1)
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -10,10 +20,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 // extra packages
 
+<<<<<<< HEAD
 // routes
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
+=======
+// routers
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+>>>>>>> ef817e3 (assignment for week9 - Jobs-api-part1)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +39,10 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+<<<<<<< HEAD
+=======
+    await connectDB(process.env.MONGO_URI)
+>>>>>>> ef817e3 (assignment for week9 - Jobs-api-part1)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
--- db/connect.js
+++ db/connect.js
@@ -3,9 +3,21 @@ const mongoose = require('mongoose')
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
+<<<<<<< HEAD
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
+=======
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  })
+  .then(() => {
+    console.log("Connected to the DB...");
+  })
+  .catch((err) => {
+    console.error("Database connection Error: ", err);
+>>>>>>> ef817e3 (assignment for week9 - Jobs-api-part1)
   })
 }
 
